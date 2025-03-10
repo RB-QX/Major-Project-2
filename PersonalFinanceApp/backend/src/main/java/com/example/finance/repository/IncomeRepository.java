0e510ef0b3d9fc3396f7e0f20ba6e1dc72f7b693
Rachit-Gamer <rachitreal14052003.rb@gmail.com>
2025-03-10 19:22:35 +0530

    Remove unused files and directories from the project

--- PersonalFinanceApp/backend/src/main/java/com/example/finance/repository/IncomeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.finance.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.finance.model.Income;
-
-public interface IncomeRepository extends JpaRepository<Income, Long> { }
