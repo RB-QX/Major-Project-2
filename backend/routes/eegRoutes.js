fbc51feff245b70ba3d17294751674efea8110a3
Rachit-Gamer <rachitreal14052003.rb@gmail.com>
2025-03-14 11:12:22 +0530

    commit

--- backend/routes/eegRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/eegRoutes.js
-const express = require('express');
-const router = express.Router();
-const { submitEegData, getAnalysis } = require('../controllers/eegController');
-
-router.post('/submit', submitEegData);
-router.get('/analysis/:userId', getAnalysis);
-
-module.exports = router;
